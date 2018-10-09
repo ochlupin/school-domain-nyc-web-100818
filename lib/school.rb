75bc64a198a9e325a7d9573a28e9e82c7be69cbe
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-10-09 17:28:09 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+
+attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name,grade)
+    @roster[grade] ||= []
+    @roster[grade].push(student_name)
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted_roster = {}
+    @roster.each do |grade,students|
+      sorted_roster[grade] = students.sort
+    end
+    sorted_roster
+  end
+end
